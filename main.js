cc253a6c63605ac2a56525dfdbde4d8cda3e2c52
daimessdn@gmail.com <daimessdn@gmail.com>
2021-01-03 12:27:02 +0700

    added service worker and register it

--- main.js
+++ main.js
@@ -121,3 +121,12 @@ noteForm.addEventListener("submit", (event) => {
 
     loadPad();
 });
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("./serviceWorker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err))
+  })
+}
